c9f10b7a319633ddeeb9d85e9031e44985fbe39b
Vijay <dhruvakumar.gopisetty@gmail.com>
2024-01-30 20:24:02 +0530

    Done Coding

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,74 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+        this.name=name;
+        this.balance=balance;
+        this.minBalance=minBalance;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
-        return null;
+        int[] freq=new int[10];
+        int count=0;
+        for(int i=9;i>=1;i--){
+            if(sum!=0 && sum>=i){
+                int frequency=sum/i;
+                freq[i]=frequency;
+                sum-=frequency*i;
+            }
+        }
+        StringBuilder accountNumber=new StringBuilder();
+        for(int i=9;i>0;i--){
+            while(freq[i]!=0){
+                accountNumber.append(i);
+            }
+        }
+        while(accountNumber.length()!=digits){
+            accountNumber.append(0);
+        }
+        if(accountNumber.length()==digits) return accountNumber.toString();
+        else{
+            throw new Exception("Account Number cannot be generated");
+        }
     }
 
     public void deposit(double amount) {
-        //add amount to balance
-
+        balance+=amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        double remainingAmount=balance-amount;
+        if(remainingAmount<minBalance){
+            throw new Exception("Insufficient Balance");
+        }
+        balance=remainingAmount;
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,8 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name,balance,5000);
+        this.tradeLicenseId=tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +14,27 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-
+        if(checkConsecutive()){
+            int n=tradeLicenseId.length();
+            int[] freq=new int[26];
+            for(int i=0;i<tradeLicenseId.length();i++){
+                char ch=tradeLicenseId.charAt(i);
+                freq[ch-'A']++;
+            }
+            for(int i=0;i<26;i++){
+                if(freq[i]>n/2){
+                    throw new Exception("Valid License can not be generated");
+                }
+            }
+        }
+    }
+    public boolean checkConsecutive(){
+        int n=tradeLicenseId.length();
+        for(int i=0;i<n-1;i++){
+            if(tradeLicenseId.charAt(i)==tradeLicenseId.charAt(i+1))
+                return false;
+        }
+        return true;
     }
 
 }
